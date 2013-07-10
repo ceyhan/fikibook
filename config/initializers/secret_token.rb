ac70b58e31760d11b7e10c9f88cd7f3fdb179ecf
Ceyhan Demir <ceyhandemir@gmail.com>
2013-07-10 21:59:41 +0300

    Created a rails application and added status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fikibook::Application.config.secret_token = '8660a9432e82aae874abb2142ea57a47836149722a1d10cb7d2faba2f1013b4a379855529a8281dd956e66132517b647c2e4705d6c64844354ce7331a2cd44a4'
